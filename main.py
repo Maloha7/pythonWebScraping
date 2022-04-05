ada1abb0987721e6d13d43ac44a3155b812c2198
Marius Hatland <marius.hatland@student.uib.no>
2022-04-05 23:08:02 +0200

    retrieved event info from echo

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from bs4 import BeautifulSoup;
+import requests
 
-with open('index.html', 'r') as html_file:
-    content = html_file.read()
+# Example to find html elements
+# with open('index.html', 'r') as html_file:
+#     content = html_file.read()
     
-    #New BeautifulSoup instance
-    bs = BeautifulSoup(content, "lxml")
+#     #New BeautifulSoup instance
+#     bs = BeautifulSoup(content, "lxml")
 
-    sections = bs.find_all('div', class_='section')
+#     sections = bs.find_all('div', class_='section')
     
-    for section in sections:
-        print(section.button.text)
+#     for section in sections:
+#         print(section.button.text)
+
+#Scraping a real website
+html_page = requests.get('https://echo.uib.no/event').text
+soup = BeautifulSoup(html_page, 'lxml')
+
+eventsDiv = soup.find('div', class_='css-jy27e')
+events = eventsDiv.find_all('p')
+linksToEvents = eventsDiv.find_all('a')
+
+i = 0
+link = 0
+while (i < len(events) - 1):
+    print(events[i].text, events[i+1].text,"https://echo.uib.no"+str(linksToEvents[link]['href']))
+    i+=2
+    link+=1
 
     
